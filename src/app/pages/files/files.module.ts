6b07ad7fe5ccb1ac95db7a3b7098d1178bc0e748
gabriel.csollei <gabriel.csollei@qbsw.sk>
2019-07-16 17:03:40 +0200

    add roles

--- src/app/pages/files/files.module.ts
+++ src/app/pages/files/files.module.ts
@@ -5,11 +5,13 @@ import { FilesRoutingModule } from "./files-routes.module";
 import { FileUploadComponent } from "./file-upload/file-upload.component";
 import { MyFilesComponent } from "./my-files/my-files.component";
 import { SharedDirectivesModule } from "../../shared/directives/shared-directives.module";
+import { MaterialModule } from "../../shared/modules/material.module";
 
 @NgModule({
     declarations: [FileListComponent, FileUploadComponent, MyFilesComponent],
     imports: [
         SharedDirectivesModule,
+        MaterialModule,
         FilesRoutingModule,
         CommonModule,
     ],
