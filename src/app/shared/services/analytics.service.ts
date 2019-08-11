a1a5f7c97cc7522221babd29530dac5ff812f111
gabriel.csollei <ext.gabriel.csolei@tieto.com>
2019-08-11 17:22:46 +0200

    finish google analytics

---
+++ src/app/shared/services/analytics.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from "@angular/core";
+
+declare const gtag: any;
+
+@Injectable({
+    providedIn: "root",
+})
+export class AnalyticsService {
+    public eventUploadFile(fileName: string): void {
+        gtag("event", "fileUpload", {fileName});
+    }
+
+    public login(method: "google"): void {
+        gtag("event", "login", {method});
+    }
+
+    public signOut(): void {
+        gtag("event", "logout");
+    }
+
+    public visitPage(page_path: string): void {
+        gtag("config", "UA-47857230-3", {page_path});
+    }
+}
