54492fd0a5706953b048300baf078adaca2c5d94
Gabriel Csollei <gcsollei@hotmail.com>
2021-04-20 09:40:49 +0200

    Refactor side menu

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { combineLatest, Observable, of } from "rxjs";
+import { map } from "rxjs/operators";
 import { AppStaticConfig } from "../../../appStaticConfig";
 import { AuthService } from "../../services/auth.service";
 import { MenuItemModel } from "../menu-item.model";
 
 @Component({
-    selector: "app-side-menu",
+    selector   : "app-side-menu",
     templateUrl: "./side-menu.component.html",
-    styleUrls: ["./side-menu.component.scss"]
+    styleUrls  : ["./side-menu.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuComponent implements OnInit {
-    public menuItems: MenuItemModel[] = AppStaticConfig.MENU_ITEMS;
+export class SideMenuComponent {
+    public readonly menuItems$: Observable<MenuItemModel[]> = combineLatest([
+        of(AppStaticConfig.MENU_ITEMS),
+        this.authService.user$,
+    ]).pipe(
+        map(([menuItems, user]) =>
+            menuItems.filter((menuItem) => !menuItem.access || (user && this.authService.canShowMenuItem(user, menuItem.access))),
+        )
+    );
 
-    public constructor(public readonly authService: AuthService) {
-    }
-
-    public ngOnInit(): void {
-    }
-
+    public constructor(private readonly authService: AuthService) {}
 }
