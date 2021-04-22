73823514dd9ead4720516a7735729805a7fa8883
Gabriel Csollei <gcsollei@hotmail.com>
2021-04-22 11:20:48 +0200

    Use google sheet as source of projects

--- src/app/pages/about/about.service.ts
+++ src/app/pages/about/about.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { of } from "rxjs";
+import { Observable, of } from "rxjs";
 import { map, shareReplay, switchMap } from "rxjs/operators";
 import { ProjectDetail } from "./project-detail";
 import { TechnologyDetail } from "./technology-detail";
@@ -9,7 +9,7 @@ import { TechnologyDetail } from "./technology-detail";
 export class AboutService {
     private readonly baseUrl$ = of("https://g43riko.github.io/data/");
 
-    public readonly projects$ = this.baseUrl$.pipe(
+    public readonly projects$ = this.fetchProjects() || this.baseUrl$.pipe(
         switchMap((url) =>
             this.httpClient.get<ProjectDetail[]>(`${url}/projects.json`, {responseType: "json"})
         ),
@@ -29,8 +29,53 @@ export class AboutService {
         shareReplay(1),
     );
 
+    private fetchProjects(): Observable<ProjectDetail[]> {
+        return this.httpClient.get(
+            "https://docs.google.com/spreadsheets/d/e/2PACX-1vSaVQnkVJvR8SNtoOQCIsC3zN9cganv5996edvnp12SceGjZZ7U7-4zLa8zYH14kOnyw4MLaoHwOc1V/pub?gid=0&single=true&output=tsv",
+            {
+                responseType: "text"
+            }).pipe(
+            map((content) => this.parseProjectsTsv(content))
+        );
+    }
+
     public constructor(
         private readonly httpClient: HttpClient,
     ) {
+    }
+
+    private parseProjectsTsv(content: string): ProjectDetail[] {
+        return content.split("\n").map((row, index) => {
+            const [name, subName, team, technologies, tests, web, docs, isPublic, finished, onHomepage, deprecated, github, NPM, codePen, active] = row.split("\t");
+
+            if (!name || !index || !onHomepage || onHomepage.toLowerCase() === "false") {
+                return undefined as any;
+            }
+
+
+            const getSource = (): ProjectDetail["sources"] => {
+                if (github) {
+                    return {
+                        url: github,
+                        service: "Github",
+                    };
+                }
+                if (codePen) {
+                    return {
+                        url: codePen,
+                        service: "Codepen",
+                    };
+                }
+            };
+
+            return {
+                name,
+                demo         : web,
+                technologies : technologies.split(",").map((e) => e.trim()),
+                team         : team as any,
+                documentation: docs,
+                sources      : getSource(),
+            };
+        }).filter((e) => !!e);
     }
 }
--- src/app/pages/about/project-detail.ts
+++ src/app/pages/about/project-detail.ts
@@ -1,11 +1,11 @@
-type Teams = "fiit" | "home" | "qbsw" | "softec" | "ibl";
+export type Teams = "FIIT" | "HOME" | "QBSW" | "SOFTEC" | "IBL";
 type Technologies = string | "NodeJS" | "Angular" | "Typescript" | "Java" | "NestJS" | "MongoJS";
 
 export interface ProjectDetail {
     readonly team: Teams;
     readonly name: string;
     readonly demo: string;
-    readonly sources?: string;
+    readonly sources?: { url: string, service: string};
     readonly documentation?: string;
     readonly technologies: readonly Technologies[];
 }
